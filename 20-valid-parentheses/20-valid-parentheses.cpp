a101e3806e3917c5d867385488731e6dc48fd1cc
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2022-09-14 00:17:55 +0530

    Time: 3 ms (47.07%), Space: 6.4 MB (50.99%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,40 +1,72 @@
 class Solution {
 public:
     bool isValid(string s) {
-            stack<char> valid;
-            if(s.size() <2) return false;
-            unordered_map<char,char> m;
-          m[')'] = '(';
-            m['}'] = '{';
-            m[']'] = '[';
             
+            
+//              stack<char> open;
+        
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] == ')' || s[i] == '}' || s[i] == ']') 
+//             {
+//                 if (open.empty()) {
+//                     return false;
+//                 }
+//                 if (s[i] == ')' && open.top() != '(') {
+//                     return false;
+//                 }
+//                 if (s[i] == '}' && open.top() != '{') {
+//                     return false;
+//                 }
+//                 if (s[i] == ']' && open.top() != '[') {
+//                     return false;
+//                 }
+//                 open.pop();
+//             }
+//                 else {
+//                 open.push(s[i]);
+//             }
+//         }
+        
+//         if (!open.empty()) {
+//             return false;
+//         }
+//         return true;
+            
+          
+            
+            stack<char> valid;
+ 
             for(int i=0;i<s.size();i++)
             {
-                    
-                    if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+                    if(s[i] == '}' || s[i] == ']' || s[i] == ')')
                     {
-                            valid.push(s[i]);
-                    }
-                    else
-                    {
+                            
                             if(valid.empty()) return false;
-                            if(valid.top() == m[s[i]])
-                            {
-                                    valid.pop();
-                            }
-                            else 
+                            if(s[i] == ')' && valid.top() != '(') 
                             {
                                     return false;
                             }
+                            if(s[i] == '}' && valid.top() != '{') 
+                            {
+                                    return false;
+                            }
+                            if(s[i] == ']' && valid.top() != '[')
+                            {
+                                    return false;
+                            }
+                            valid.pop();
+                           
                     }
-                    
-                    
+                    else
+                    {
+                            valid.push(s[i]);
+                    }
             }
             
-           
-            if(valid.empty()) return true;
-            else return false;
-                 
+             if (!valid.empty()) {
+            return false;
+        }
+        return true;
         
     }
 };
