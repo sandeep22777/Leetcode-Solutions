497c9e970686e08f630824ea0fb1548870698896
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2022-09-14 16:51:21 +0530

    Time: 264 ms (56.28%), Space: 45.5 MB (80.83%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,35 +2,57 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
-            if(nums.size() == 0) return 0;
+//             if(nums.size() == 0) return 0;
           
-            unordered_set<int> s;
-            for(int i=0;i<nums.size();i++)
-            {
-                    s.insert(nums[i]);
-            }
+//             unordered_set<int> s;
+//             for(int i=0;i<nums.size();i++)
+//             {
+//                     s.insert(nums[i]);
+//             }
 
-            int ans = 0;
-        int  c=0;
-            for(int i=0;i<nums.size();i++)
-            {
-                    c=0;
+//             int ans = 0;
+//         int  c=0;
+//             for(int i=0;i<nums.size();i++)
+//             {
+//                     c=0;
                     
-                    if(s.find(nums[i] -1) == s.end() )
-                    {
-                             c++;     
-                            int j=1;
-                            while((s.find(nums[i] +j) != s.end()) == true)
-                            {
-                                    j++;
-                                    c++;
-                            }
+//                     if(s.find(nums[i] -1) == s.end() )
+//                     {
+//                              c++;     
+//                             int j=1;
+//                             while((s.find(nums[i] +j) != s.end()) == true)
+//                             {
+//                                     j++;
+//                                     c++;
+//                             }
                           
-                    }                    
-                ans = max(ans,c);    
+//                     }                    
+//                 ans = max(ans,c);    
                     
-            }
+//             }
             
-           return ans; 
+//            return ans; 
+            
+            
+            unordered_set<int>s(nums.begin(), nums.end());
+        int longest = 0;
+        for(auto &n: s){
+            //if this is the start of the sequence
+            if(!s.count(n - 1)){
+                int length = 1; 
+                while(s.count(n + length))
+                    ++length;
+                longest = max(longest, length);
+            } 
+
+        }
+        return longest;
+            
+            
+            
+            
+            
+            
+            
     }
 };
