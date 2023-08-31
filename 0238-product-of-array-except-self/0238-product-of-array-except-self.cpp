4a4b368416493a1053a1fe61674903acbab3bc51
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2023-09-01 02:16:28 +0530

    Time: 24 ms (43.57%), Space: 26.2 MB (5.00%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,25 +2,37 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
-        
-        int s = nums.size();
-        
-        vector<int> v(s,1);
-        int prefix = 1;
-        
-        for(int i=0;i<nums.size();i++)
+        int size = nums.size();
+      vector<int> result(size,1);
+        vector<int> res(size,1);
+        vector<int> ans;
+        int sum =1;
+        result[0]  = nums[0];
+        res[size-1] = nums[size-1];
+      for(int i=1;i<nums.size();i++)
+      {
+          result[i] = nums[i] *  result[i-1];
+      }
+        for(int i= size-2;i>=0;i--)
         {
-            v[i] = prefix;
-            prefix = prefix * nums[i];
+            res[i] = nums[i] *  res[i+1];
         }
         
-        int postfix = 1;
-        for(int i= s-1;i>=0;i--)
+        for(int i=0;i<size;i++)
         {
-            v[i] = v[i] * postfix;
-            postfix = postfix * nums[i];
+            if(i==0)
+            {
+                ans.push_back(res[i+1]);
+            }
+            else if(i == size-1)
+            {
+                ans.push_back(result[i-1]);
+            }
+            else {
+                ans.push_back(result[i-1] * res[i+1]);
+            }
         }
+        return ans;
         
-        return v;
     }
 };
