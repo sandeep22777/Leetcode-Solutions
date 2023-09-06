caa8b37af45646ac0a244e2a4b78e4564e465157
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2023-09-07 02:53:11 +0530

    Time: 4 ms (75.96%), Space: 7.1 MB (98.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -6,27 +6,26 @@ public:
         int st =0;
         int e = s.size()-1;
         
-        while(st<e)
-        {
-            if(ispunct(s[st]) || s[st] ==' ')
-            {
-                st++;
-                continue;
-            }
-            else if(ispunct(s[e]) || s[e] == ' ')
-            {
-                e--;
-                continue;
-            }
-            else if(tolower(s[st]) != tolower(s[e]))
-            {
-                return false;
-            }
-            else {
-                st++;
-                e--;
-            }
-        }
+      while(st<e)
+      {
+          if(ispunct(s[st])  || s[st] ==' ')
+          {
+              st++;
+              continue;
+          }
+          else if(ispunct(s[e])  || s[e] ==' ') {
+              e--;
+              continue;
+          }
+          else if(tolower(s[st]) != tolower(s[e])) {
+              return false;
+          } 
+         else {
+                  st++;
+                  e--;
+              }
+          }
+      
         
         
         return true;
