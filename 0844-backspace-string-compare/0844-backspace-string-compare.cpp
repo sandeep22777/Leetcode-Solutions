0bc8a60ac382d98ae6857c7440dc0877107dd2f4
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2023-09-08 00:51:56 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (72.03%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,53 +1,16 @@
 class Solution {
 public:
     
-    bool backspaceCompare(string s, string t) {
-        stack<int> s1;
-        stack<int> s2;
-        
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]!= '#')
-            {
-                s1.push(s[i]);
-            }
-            else {
-                if(s1.size() !=0)
-                {
-                     s1.pop();
-                }
-               
-            }
+  string build(string s){
+        int c=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='#') c=max(0,--c);
+            else s[c++]=s[i];
         }
-        
-          for(int i=0;i<t.size();i++)
-        {
-            if(t[i]!= '#')
-            {
-                s2.push(t[i]);
-            }
-            else {
-                if(s2.size() !=0)
-                {
-                     s2.pop();
-                }
-            }
-        }
-        if(s1.empty() && !s2.empty()) return false;
-        if(s2.empty() && !s1.empty()) return false;
-        if(s1.size() != s2.size()) return false;
-        
-        while(s1.size())
-        {
-            if(s1.top() != s2.top() )
-            {
-                return false;
-            }
-            else {
-                s1.pop();
-                s2.pop();
-            }
-        }
-        return true;
+        return s.substr(0,c);
     }
+    bool backspaceCompare(string s, string t) {
+        return build(s)==build(t);
+    }
+    
 };
