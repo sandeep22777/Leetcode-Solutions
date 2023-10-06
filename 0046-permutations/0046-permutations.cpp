fcdd66c314defaea7ba1a7d2da427126c82f5cf3
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2023-10-06 14:06:32 +0530

    Time: 0 ms (100.00%), Space: 8 MB (48.57%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
      void per(vector<vector<int>> &ans,vector<int> &v,vector<int> &nums,vector<int> &m)
         {
-                if(v.size() == nums.size())
-                {
-                        ans.push_back(v);
-                        return;
-                }
-               
-                for(int i=0;i<nums.size();i++)
-                {
-                        if(!m[i])
-                        {
-                                v.push_back(nums[i]);
-                                m[i]=1;
-                                per(ans,v,nums,m);
-                                m[i]=0;
-                                v.pop_back();
-                        
-                        }
-                
-                }
-                
-                
-                
+               if(v.size() == nums.size())
+               {
+                   ans.push_back(v);
+                   return;
+               }
+         
+         for(int i= 0;i<nums.size();i++)
+         {
+             if(!m[i])
+             {
+                 v.push_back(nums[i]);
+                 m[i] = 1;
+                 per(ans,v,nums,m);
+                 m[i] = 0;
+                 v.pop_back();
+             }
+         }
                 
         }
     
