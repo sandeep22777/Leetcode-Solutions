eff04e00398d9f401e58521fda86c5b4390213e7
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2023-10-30 18:12:45 +0530

    Time: 25 ms (44.21%), Space: 23.7 MB (26.75%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,22 +11,42 @@
 class Solution {
 public:
     
-     TreeNode* LCA(TreeNode* root, TreeNode* p, TreeNode* q) {
-         if(root == NULL) return NULL;
-         if(root == p || root == q) return root;
-         
-         TreeNode* l = LCA(root->left,p,q);
-        TreeNode* r = LCA(root->right, p ,q);
-         
-         if(l != NULL && r != NULL) return root;
-         
-         return l == NULL ? r : l;
-     }
+//      TreeNode* LCA(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if(root ==  NULL) return NULL;
+//          if(root->val < p->val && root->val < q->val)
+//          {
+//              LCA(root->right,p,q);
+//          }
+//          else if(root->val > p->val && root->val > q->val)
+//          {
+//              LCA(root->left,p,q);
+//          }
+//          else {
+//               return root;
+//          }
+        
+//      }
+     TreeNode* lcs(TreeNode* root, TreeNode* p, TreeNode* q)
+        {
+               if(!root) return NULL;
+                if(root->val > p->val && root->val > q->val)
+                {
+                      return  lcs(root->left,p,q);
+                }
+                else if(root->val < p->val && root->val < q->val)
+                {
+                      return  lcs(root->right,p,q);
+                }
+                else
+                {
+                        return root;
+                }
+        }
          
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
        // if(root->left->val == p->val && root->right->val == q->val ) return root;
         
-        return LCA(root,p,q);
+        return lcs(root,p,q);
     }
 };
