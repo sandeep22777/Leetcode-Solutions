52b9b147affbe24742927fa866101aac216a08de
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2023-11-09 18:34:05 +0530

    Time: 97 ms (52.09%), Space: 93.5 MB (85.46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,20 +5,14 @@ public:
         if(n ==1 ) return 0;
         int result = 0 ;
         int min = prices[0];
-        vector<int> dp(n,-1);
-        dp[0] =  0;
-        
         for(int i= 1;i<n;i++)
         {
             if(prices[i] < min )
             {
                 min  = prices[i];
-                // result =  0;
-                dp[i] = 0;
                 continue;
             }
             result = max(prices[i] - min,result);
-            dp[i]  = result;
             
         }
         return result;
