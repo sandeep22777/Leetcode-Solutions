2ee412357df9634d93a86affc0f72d3d31b9d416
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2024-08-27 02:52:58 +0530

    Time: 197 ms (35.58%), Space: 146.3 MB (24.57%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -4,15 +4,15 @@ public:
      unordered_map<int,int> m;
        int ans= 0;
         int cur = 0;
-        m[0] = 0;
+        
        for(int i=0;i<nums.size();i++)
        {
            cur += nums[i];
-           if(i >=1 &&cur%k == 0) return true;
+           if(i>=1 && cur%k ==0) return true;
            if(m.find(cur%k) != m.end())
            {
                if(i-m[cur%k] >=2) return true;
-            }
+           }
           if(m.find(cur%k) == m.end()) m[cur%k] = i;
        }
         return false;
