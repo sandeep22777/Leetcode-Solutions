a73bd465e1ab91b15e9ef4a70bc743a72568346c
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2024-09-01 01:23:07 +0530

    Time: 215 ms (24.26%), Space: 146.3 MB (17.29%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-     unordered_map<int,int> m;
-       int ans= 0;
-        int cur = 0;
+        unordered_map<int,int> m;
+        int cur=0;
         
-       for(int i=0;i<nums.size();i++)
-       {
-           cur += nums[i];
-           if(i>=1 && cur%k ==0) return true;
-           if(m.find(cur%k) != m.end())
-           {
-               if(i-m[cur%k] >=2) return true;
-           }
-          if(m.find(cur%k) == m.end()) m[cur%k] = i;
-       }
+        for(int i=0;i<nums.size();i++)
+        {
+            cur += nums[i];
+            if(i>=1 && cur% k == 0) return true;
+            if(m.find(cur%k) != m.end())
+            {
+                if(i-m[cur%k]>=2) return true;
+            }
+         if(m.find(cur%k) == m.end()) m[cur%k] = i;
+        }
+        
         return false;
     }
 };
-
-// [23,25,29,35,42]
