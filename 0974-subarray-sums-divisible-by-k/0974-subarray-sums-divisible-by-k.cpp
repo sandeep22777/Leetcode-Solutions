8268434390b9dd8868b4852af8087ebb8de96f13
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2024-09-01 01:04:19 +0530

    Time: 44 ms (46.34%), Space: 35.6 MB (52.88%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-//        unordered_map<int,int> m;
-//         int ans =0;
-//         int cur =0;
+              
+         unordered_map<int,int> m;
+        m[0] =1;
+        int count=0;
+        int cur=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            cur += nums[i];
+            if(cur < 0) cur = cur%k + k;
+            if(m.find(cur%k) != m.end())
+            {
+                count += m[cur%k];
+            }
+            
+            m[cur%k]++;
+        }
+        
+        return count;
+        
+              
+//          unordered_map<int,int> m;
+//         m[0] = 1;
+//         int count = 0;
+        
+//         int pre = 0;
         
 //         for(int i=0;i<nums.size();i++)
 //         {
-//             cur += nums[i];
-//              if(cur < 0) cur += k;
-//             if(m.find(cur%k) != m.end())
+//             pre += nums[i];
+//             int temp = pre%k;
+//            // if(pre == 0) count++;
+//             if(temp < 0) temp += k;
+//             if(m.find(temp) != m.end())
 //             {
-//                 ans += m[cur%k];
+//                 count += m[temp];
 //             }
-//             m[cur%k]++;
+//             m[temp]++;
 //         }
-//         return ans;
-        
-              
-         unordered_map<int,int> m;
-        m[0] = 1;
-        int count = 0;
-        
-        int pre = 0;
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            pre += nums[i];
-            int temp = pre%k;
-           // if(pre == 0) count++;
-            if(temp < 0) temp += k;
-            if(m.find(temp) != m.end())
-            {
-                count += m[temp];
-            }
-            m[temp]++;
-        }
-        return count;
+//         return count;
     }
 
 };
