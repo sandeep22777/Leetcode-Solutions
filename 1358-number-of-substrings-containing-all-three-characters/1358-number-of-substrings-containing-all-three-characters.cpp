6bf3e9faade6c834fdf87c2a4ced122b6d73340c
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2024-09-05 03:28:30 +0530

    Time: 75 ms (5.27%), Space: 17.8 MB (6.67%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-        int ans= 0;
-        unordered_map<char,int> m;
-        int j=0;
-        int len = s.size();
-        for(int i=0;i<len;i++)
+     int findatmostk(string nums,int k)
+    {
+        int i=0;
+        int j =0;
+        int ans =0;
+        unordered_map<int,int>m;
+        for(int j=0;j<nums.size();j++)
         {
-            m[s[i]]++;
-            while(m.size() == 3)
+            m[nums[j]]++;
+            while(m.size() > k)
             {
-                ans += len-i;
-                m[s[j]]--;
-                if(m[s[j]] == 0)
-                {
-                    m.erase(s[j]);
-                }                
-                j++;
+                m[nums[i]]--;
+                if(m[nums[i]] == 0) m.erase(nums[i]);
+                i++;
             }
-           
+            
+            ans += j-i+1;
         }
+        
         return ans;
+    }
+    
+    int numberOfSubstrings(string s) {
+//         int ans= 0;
+//         int i=0;
+//         unordered_map<char,int> m;
+//         string cur;
+//         for(int j=0;j<s.size();j++)
+//         {
+//             m[s[j]]++;
+            
+//         }
+        int atmostk = findatmostk(s,3);
+      int atmostk2 = findatmostk(s,2);
+        return atmostk - atmostk2;
     }
 };
