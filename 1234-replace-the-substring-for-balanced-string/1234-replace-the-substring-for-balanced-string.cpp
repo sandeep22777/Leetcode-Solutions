4f938c3d98926a4e6e6cbd343451fa427178fa85
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2024-09-06 00:39:56 +0530

    Time: 43 ms (20.97%), Space: 10.6 MB (21.51%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,25 +1,55 @@
 class Solution {
 public:
     int balancedString(string s) {
-        int how  = s.size()/4;
-        
         unordered_map<char,int> m;
         for(int i=0;i<s.size();i++)
         {
             m[s[i]]++;
         }
-        int ans= s.size();
+        int ans =INT_MAX;
+        int n  = s.size()/4;
         int j=0;
+        if (m['Q'] == n && m['W'] == n && m['E'] == n && m['R'] == n) {
+        return 0;
+    }
         for(int i=0;i<s.size();i++)
         {
-            m[s[i]]--;  
-            while(j< s.size() &&  m['Q'] <= how && m['R'] <= how && m['W'] <= how && m['E'] <= how)
+           m[s[i]]--;
+            
+            while(  m['Q'] <= n && m['R'] <= n && m['E'] <= n && m['W'] <= n )
             {
-                ans= min(ans, i-j+1);
-                m[s[j++]]++;
+                ans = min(ans,i-j+1);
+                m[s[j]]++;
+                j++;
             }
         }
-        return ans;
+        return ans == INT_MAX ? 0 : ans;
+     
+        
+//         for(int i=0;i<s.size();i++)
+//         {
+//            if( m[s[i]] >= n ) {
+             
+//             ans += m[s[i]] -n;  
+//                  m[s[i]] = 0;
+//            }
+//         }
+//         return ans;
         
     }
 };
+// n- 5
+
+// w - 8
+// e - 4
+// q - 5
+// r - 3    
+
+// n - 2
+    
+// w - 3
+// e - 0
+// q - 4
+// r - 1    
+    
+    
