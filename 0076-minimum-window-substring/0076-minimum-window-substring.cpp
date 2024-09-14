e3de71e1e65c0bd2bdffd1febb89308fd0691d13
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2024-09-14 16:27:14 +0530

    Time: 20 ms (61.06%), Space: 11.1 MB (39.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(s.size() < t.size()) return "";
-        int temp = t.size();
         unordered_map<char,int> m;
+        int high = INT_MAX;
+        int start =0;
+        
         for(auto i : t) m[i]++;
-        int j=0;
-        int high =INT_MAX;
-        int first, second =0;
+        int len = t.size();
+        int count = len;
+        int j = 0;
         
         for(int i=0;i<s.size();i++)
         {
-            if(m[s[i]]> 0)
-            {
-                temp--;
-             
-            }
-               m[s[i]]--;
             
-            while(temp ==0)
+            if(m[s[i]] > 0)
             {
-                if(i-j+1 < high)
+                count--;
+            }
+            m[s[i]]--;
+            while(count == 0)
+            {
+                if(i-j < high)
                 {
-                    high = min(high,i-j+1);
-                    first = j;
+                    high = i-j+1;
+                    start = j;
                 }
                 m[s[j]]++;
                 if(m[s[j]] > 0)
                 {
-                    temp++;                               
+                    count++;
                 }
-                 j++;
-                cout<< j << endl;
+                j++;
             }
+                        
         }
-        if(high != INT_MAX) return s.substr(first,high);
-        else return "";
-       
+        if(high == INT_MAX) return "";
+        else return s.substr(start,high);
     }
 };
