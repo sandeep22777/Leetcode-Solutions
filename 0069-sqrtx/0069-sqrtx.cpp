dde1abbc1aacc2586b7026d56aa7dfb6d89468d1
sandeep22777 <72358529+sandeep22777@users.noreply.github.com>
2024-10-14 21:33:16 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (18.12%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        if(x ==0 ) return 0;
-        if(x < 4) return 1;
-        long long s = 0;
-        long long e  = x;
+    int mySqrt(int n) {
+//         if(x ==0 ) return 0;
+//         if(x < 4) return 1;
+//         long long s = 0;
+//         long long e  = x;
         
-        while(s <=e)
+//         while(s <=e)
+//         {
+//             long long mid = s+ (e-s)/2;
+//             if(mid == x/mid) return mid;
+//             else if( mid* mid < x) s = mid+1;
+//                 else e = mid-1;
+//         }
+//         return s-1;
+        if(n ==0) return 0;
+         long long s = 1;
+    long long e = n;
+    int ans = 1;
+
+    while(s<=e)
+    {
+        long long m = s + (e-s)/2;
+        // if(m*m == n) return m;
+
+        if(m*m > n)
         {
-            long long mid = s+ (e-s)/2;
-            if(mid == x/mid) return mid;
-            else if( mid* mid < x) s = mid+1;
-                else e = mid-1;
+            e = m-1;
         }
-        return s-1;
+        else {
+            ans = m;
+            s= m+1;
+        }
+    }
+    return ans;
     }
 };
